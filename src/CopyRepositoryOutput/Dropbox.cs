013158b3dec81d9187c50f8b5f5e95c8b4057047
Michael Allen <michael.allen@cognex.com>
2015-07-16 09:57:56 -0500

    Use the updated version of the Dropbox helper

--- src/CopyRepositoryOutput/Dropbox.cs
+++ src/CopyRepositoryOutput/Dropbox.cs
@@ -4,30 +4,31 @@ using System.Text;
 
 namespace System.Common.References
 {
-  /// <summary>
-  /// 
-  /// </summary>
   public static class Dropbox
   {
-    /// <summary>
-    /// 
-    /// </summary>
     private static Lazy<string> location = new Lazy<string>(() =>
     {
-      string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-      string hostdb = Path.Combine(appData, @"Dropbox\host.db");
-      string[] lines = File.ReadAllLines(hostdb);
-      return Encoding.ASCII.GetString(Convert.FromBase64String(lines[1]));
+      var folders = new[]
+      {
+        Environment.SpecialFolder.ApplicationData,
+        Environment.SpecialFolder.CommonApplicationData,
+        Environment.SpecialFolder.LocalApplicationData,
+      };
+
+      foreach (var folder in folders)
+      {
+        string appData = Environment.GetFolderPath(folder);
+        string hostdb = Path.Combine(appData, @"Dropbox\host.db");
+        if (!File.Exists(hostdb)) continue;
+        string[] lines = File.ReadAllLines(hostdb);
+        return Encoding.ASCII.GetString(Convert.FromBase64String(lines[1]));
+      }
+
+      throw new FileNotFoundException("host.db not found");
     }, true);
 
-    /// <summary>
-    /// 
-    /// </summary>
     public static string Location { get { return location.Value; } }
 
-    /// <summary>
-    /// 
-    /// </summary>
     static Dropbox() { }
   }
 }
